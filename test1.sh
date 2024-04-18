e2611e9c4f61f139db68485f30bb1b9d7e440e99
Ebubekir <ekaramustafa20@ku.edu.tr>
2024-04-18 16:42:15 +0200

    example test

---
+++ test1.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# Define colors
+GREEN='\e[32m'
+RED='\e[31m'
+RESET_COLOR='\e[0m'
+
+# Change directory to ~/dm510fs-mountpoint/
+cd ~/dm510fs-mountpoint/
+
+expected_string="a" 
+
+ls_output=$(ls)
+
+echo "================================"
+echo "Test 1"
+echo "The output: $ls_output"
+echo "Expected output: $expected_string"
+
+# Compare ls output with the expected string
+if [ "$ls_output" = "$expected_string" ]; then
+    echo -e "${GREEN}Test 1 Success${RESET_COLOR}"
+else
+    echo -e "${RED}Test 1 Fail${RESET_COLOR}"
+fi
+echo "================================"
+echo
